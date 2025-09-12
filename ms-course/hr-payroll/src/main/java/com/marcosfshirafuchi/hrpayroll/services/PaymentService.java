a86cf199128cb79348db703c61e21e6331678db7
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-09-11 23:18:32 -0300

    RestTemplate

--- ms-course/hr-payroll/src/main/java/com/marcosfshirafuchi/hrpayroll/services/PaymentService.java
+++ ms-course/hr-payroll/src/main/java/com/marcosfshirafuchi/hrpayroll/services/PaymentService.java
@@ -1,13 +1,28 @@
 package com.marcosfshirafuchi.hrpayroll.services;
 
 import com.marcosfshirafuchi.hrpayroll.entities.Payment;
+import com.marcosfshirafuchi.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    public Payment getPayment(long workedId, int days){
+    @Value("${hr-worker.host}")
+    private String workerHost;
 
-        return new Payment("Bob",200.0,days);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public Payment getPayment(long workedId, int days){
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", "" + workedId);
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(),worker.getDailyIncome(),days);
     }
 }
