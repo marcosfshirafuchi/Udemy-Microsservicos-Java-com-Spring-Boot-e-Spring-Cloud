0f4f15bf70e5e3686e96423c3771ea2439a0601f
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-11-09 17:15:11 -0300

    Atualização da pasta: Seção 05 - Fase 4 - Autenticação e autorização

---
+++ Seção 05 - Fase 4 - Autenticação e autorização/46. Configuração de CORS/hr-oauth/src/test/java/com/marcosfshirafuchi/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,15 @@
+package com.marcosfshirafuchi.hroauth;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HrOauthApplicationTests {
+
+    @Test
+    void sanity() {
+        // Teste leve, não sobe contexto do Spring
+        assertTrue(true);
+    }
+}
+
+
